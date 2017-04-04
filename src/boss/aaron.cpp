22590251fd99fa435d23d2d2073dc79e4b1961d9
Jannik Koch <Jannik.Koch@protonmail.com>
2017-04-04 16:33:26 +0200

    Tweaks health of the first three bosses

--- src/boss/aaron.cpp
+++ src/boss/aaron.cpp
@@ -2,7 +2,7 @@
 
 namespace los {
 	Aaron::Aaron(SDL_Renderer *renderer) : Boss(renderer, 2, "../res/boss/aaron.png", 0, -300) {
-		m_health = 800;
+		m_health = 600;
 	}
 	Aaron::~Aaron() {}
 };
--- src/boss/jannik.cpp
+++ src/boss/jannik.cpp
@@ -2,7 +2,7 @@
 
 namespace los {
 	Jannik::Jannik(SDL_Renderer *renderer) : Boss(renderer, 3, "../res/boss/jannik.png", 0, -300) {
-		m_health = 1500;
+		m_health = 600;
 	}
 	Jannik::~Jannik() {}
 };
